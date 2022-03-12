7620a8a901937006cd2b47469d4d780440ba1786
RYE-513 <93022160+RYE-513@users.noreply.github.com>
2022-03-12 12:52:52 +0800

    first time to commit python

---
+++ AMONGUS.py
@@ -0,0 +1,9 @@
+encrypt = 'ryebrizbscs'
+
+print('original: '+encrypt)
+encrypt = encrypt[::-1]
+print(encrypt)
+encrypt = encrypt.replace('e','b')
+encrypt = encrypt.replace('r','mm')
+encrypt = encrypt.replace('s','phd')
+print(encrypt)
